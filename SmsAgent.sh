cfe0062c72a0d0ca7c33c8ee485875a86a809b7f
jlardouin <jlardouin@gmail.com>
2022-06-26 07:42:43 +0000

    Version 0.0.7 with 3rd version of SmsAgent

--- SmsAgent.sh
+++ SmsAgent.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Script to install SMS Agent on a Linux Machine
 # - to store in OBS s3://obs-jla-repo
+# to execute through cloudinit running the following command
+#   sudo bash -c  "curl https://obs-jla-repo.oss.eu-west-0.prod-cloud-ocb.orange-business.com/SmsAgent.sh | tee SmsAgent.sh && chmod 755 SmsAgent.sh && ./SmsAgent.sh"
 echo "----------------------------------"
 echo "Starting SMS Agent installation..."
 echo "----------------------------------"
